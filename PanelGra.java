6bf62602d78d0cc5955d1e97a9334dbc354e5bf3
mwprojekt <48382851+mwprojekt@users.noreply.github.com>
2020-03-19 22:45:58 +0100

    Update PanelGra.java

--- PanelGra.java
+++ PanelGra.java
@@ -1,13 +1,58 @@
-import java.awt.Color;
-
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.*;
 import javax.swing.JPanel;
+import javax.swing.*;
+
 
 public class PanelGra extends JPanel{
-
+	
+	private Statek s = new Statek();
+	private int x, y;
+	private int shootX, shootY;	
+	
 	PanelGra(){
 		this.setBackground(Color.black);
+		 SpaceshipListener monitor = new SpaceshipListener();
+		 
+		 addMouseMotionListener(monitor);
+	     addMouseListener(monitor);
 		
 	}
+
 	
-	
+	   public void paintComponent(Graphics g)
+	   {
+		  //rysuje statek
+	      super.paintComponent(g);
+	      s.draw(g,1000);
+	          
+	   }
+	private class SpaceshipListener implements MouseListener, MouseMotionListener
+	   {
+	      public void mouseMoved(MouseEvent event)
+	      {
+	         s.move(event.getX(), event.getY());		
+	         repaint();
+	      }
+	      
+	      public void mousePressed(MouseEvent event)
+	      {
+	         s.setShooting(true);					
+	         repaint();
+	      }
+
+	      public void mouseReleased(MouseEvent event)
+	      {
+	         s.setShooting(false);				
+	         shootX =0;
+	         shootY =0;
+	         repaint();
+	      }
+	      public void mouseDragged(MouseEvent event){}
+	      public void mouseClicked(MouseEvent event) {}
+	      public void mouseEntered(MouseEvent event) {}
+	      public void mouseExited(MouseEvent event) {}
+	   }
 }
